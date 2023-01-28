a04903bc358515eb44f93aac7e3d4a0d37568fed
CHEN <812607138@qq.com>
2023-01-28 19:17:54 +0800

    views

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,16 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
         <Switch>
           <Route path="/tags">
             <Tags />
@@ -39,29 +28,8 @@ function App() {
             <NoMatch/>
           </Route>
         </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
     </Router>
   );
-}
-
-function NoMatch(){
-  return (
-    <div>页面不存在</div>
-  )
-}
-
-function Statistics() {
-  return <h2>统计页</h2>;
-}
-
-function Tags() {
-  return <h2>标签页</h2>;
-}
-
-function Money() {
-  return <h2>记账页</h2>;
 }
 
 export default App;
